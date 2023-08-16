cb6164478893fe7d785a483771731acc2f07d84b
unknown <you@example.com>
2023-08-16 12:02:15 +0530

    verify-otp

--- backend/routes/otpRoute.js
+++ backend/routes/otpRoute.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const {sendOTP} = require("../otpcontoller/controller")
+const {sendOTP, verifyOtp} = require("../otpcontoller/controller")
 
+
+router.post('/verify', async(req, res)=>{
+  try{
+    let {email, otp} = req.body
+    const validOTP = await verifyOtp({email, otp})
+    res.status(200).json({valid: validOTP})
+  }catch(error){
+    res.status(404).send(error.message)
+  }
+})
 router.post('/otp', async (req, res) => {
   try {
    const {email, subject, message, duration} = req.body
