93255c33f148d3d14e20fdda5a98054c3200123c
unknown <you@example.com>
2023-08-16 12:13:42 +0530

    backend

--- backend/routes/otpRoute.js
+++ backend/routes/otpRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {sendOTP, verifyOtp} = require("../otpcontoller/controller")
+const {sendOTP, verifyOtp} = require("../controller/otpController")
 
 
 router.post('/verify', async(req, res)=>{
